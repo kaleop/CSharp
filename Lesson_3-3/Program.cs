4f057031518fb87f55732d37c14d9f59d7f22e07
Victor <kaleop@mail.ru>
2021-05-22 17:17:20 +0300

    Третий урок третье задание

---
+++ Lesson_3-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string s = Console.ReadLine();
+
+            char[] s2 = s.ToCharArray();
+
+            for (int i = s2.Length-1 ; i >= 0 ; i--)
+            {
+                Console.Write(s2[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
