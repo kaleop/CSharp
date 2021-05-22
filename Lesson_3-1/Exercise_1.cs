5cfa8a2c7abebb5e3b9d6bb305de53d3223821a3
Victor <kaleop@mail.ru>
2021-05-22 15:28:12 +0300

    Третий урок первое задание

---
+++ Lesson_3-1/Exercise_1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lesson_3_1
+{
+    class Exercise_1
+    {
+        static void Main(string[] args)
+        {
+            int[,] mass = new int[5,5];
+            int[,] mass2 = new int[2, 5];
+
+            Random r = new Random();
+
+            Console.WriteLine(mass.GetUpperBound(0));
+            Console.WriteLine(mass.GetUpperBound(1));
+
+            for (int i = 0; i <= mass.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= mass.GetUpperBound(1); j++)
+                {
+                    mass[i, j] = r.Next(0,10);
+                    Console.Write(mass[i,j] + " ");
+
+                    // Первая Диагональ
+                    if (i == j)                         
+                    {
+                        mass2[0, i] = mass[i, j];
+                    }
+
+                    // Вторая диагональ
+                    if ( (i+j) == mass.GetUpperBound(1)) 
+                    {
+                        mass2[1, i] = mass[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= mass2.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= mass2.GetUpperBound(1); j++)
+                {
+                    Console.Write(mass2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
