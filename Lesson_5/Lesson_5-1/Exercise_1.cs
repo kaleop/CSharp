014fdea1bb0db78469e49f954b0366d40286b832
Victor <kaleop@mail.ru>
2021-05-30 13:12:34 +0300

    Первое задание пятого урока

---
+++ Lesson_5/Lesson_5-1/Exercise_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson_5_1
+{
+    class Exercise_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите и нажмите Enter:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Введите название файла:");
+            string fileName = Console.ReadLine() + ".txt";
+
+            File.WriteAllText(fileName, text);
+            Console.ReadKey();
+        }
+    }
+}
