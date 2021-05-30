28b70f2a2a7a7e63927fe32a3af1829c5a729c89
Victor <kaleop@mail.ru>
2021-05-30 13:24:22 +0300

    Второе задание пятого урока

---
+++ Lesson_5/Lesson_5-2/Exercise_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson_5_2
+{
+    class Exercise_2
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("startup.txt", DateTime.Now.ToString()+ "\n");
+        }
+    }
+}
