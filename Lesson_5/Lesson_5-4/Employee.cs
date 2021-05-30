fd176b654dab4d85b6c7c80e66c9d2b0dfcbc996
Victor <kaleop@mail.ru>
2021-05-30 16:00:27 +0300

    Четвёртое задание пятого урока

---
+++ Lesson_5/Lesson_5-4/Employee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_4
+{
+    class Employee
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public string Patronymic { get; }
+        public int Age { get; }
+        private Post post;
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Employee(string name, string surname, string patronymic, string email, string phoneNumber, int age, Post job)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Age = age;
+            post = job;
+        }
+
+
+        public void EmployeeInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Сотрудник:");
+            Console.ResetColor();
+            Console.WriteLine($"\n{Surname} {Name} {Patronymic} {Age}\n{this.post.JobTitle} с зарплатой {this.post.Income} \n{Email} {PhoneNumber}");
+        }
+
+    }
+
+    class Post
+    {
+        private double income;
+        private int workExperience = 0;
+
+        public string JobTitle { get; set; }
+        public double Income 
+        {
+            get { return income; }
+
+            set
+            {
+                income = value * (1 + workExperience / 100);
+            }
+        }
+        public int WorkExperience 
+        { 
+            get
+            {
+                return workExperience;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    workExperience = 0;
+                }
+                else
+                {
+                    workExperience = value;
+                }
+            }
+        }
+
+        public Post (double income, string jobTitle) : this(0,income, jobTitle)
+        {
+
+        }
+
+        public Post (int workExperience, double income, string jobTitle)
+        {
+            WorkExperience = workExperience;
+            Income = income;
+            JobTitle = jobTitle;
+        }
+
+    }
+}
---
+++ Lesson_5/Lesson_5-4/Exercise_4.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson_5_4
+{
+    class Exercise_4
+    {
+        static void Main(string[] args)
+        {
+            Employee[] emp = new Employee[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                emp[i] = EmpCreate();
+            }
+
+            int count = 0;
+
+            foreach (Employee e in emp)
+            {
+                if (e.Age >= 40)
+                {
+                    e.EmployeeInfo();
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Все сотрудники младше 40 лет");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static Employee EmpCreate ()
+        {
+            string[] names = new string[6]
+                {"Антон", "Пётр", "Игнат", "Захар", "Евгений", "Александр"};
+
+            string[] surnames = new string[6]
+                {"Чижиков", "Цибуля", "Орешкин", "Мутко", "Оганесян", "Чёрный"};
+
+            string[] patronymics = new string[6]
+                {"Халкович", "Торович", "Айронмэнович", "Питерпаркерович", "Пантерович", "Веномович"};
+
+
+            string name = RandomString(names);
+
+            Random r = new Random();
+
+
+            return new Employee(name, RandomString(surnames), RandomString(patronymics), RandomEmail(name), RandomPhone(), r.Next(18, 65), RandomPost());
+        }
+
+        static string RandomPhone ()
+        {
+            StringBuilder number = new StringBuilder("+7");
+            Random r = new Random();
+            for (int i=0; i<9; i++)
+            {
+                number.Append(r.Next(0, 9).ToString());
+            }
+            return number.ToString();
+        }
+
+        static string RandomString (string[] FIOArray)
+        {
+            Random r = new Random();
+            return FIOArray[r.Next(0, FIOArray.Length - 1)];
+        }
+
+        static string RandomEmail(string name)
+        {
+            string[] email = new string[4]
+                {"@mail.ru","@yandex.ru","@gmail.ru","@yahoo.ru"};
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(name);
+            sb.Append(RandomString(email));
+            return sb.ToString();
+        }
+
+        static Post RandomPost()
+        {
+            string[] job = new string[7]
+                {"Работяга","Инженер","Менеджер","Охранник","Сын маминой подруги", "Программист","ОТК"};
+
+            Random r = new Random();
+
+            return new Post(r.Next(0, 25), r.Next(30000,150000), job[r.Next(0, 6)]);
+        }
+    }
+}
