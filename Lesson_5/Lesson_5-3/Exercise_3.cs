f7a7c4d9d6b1bd5cd6d473c08e4619d1dd4f8c05
Victor <kaleop@mail.ru>
2021-05-30 13:40:06 +0300

    Третье задание пятого урока

---
+++ Lesson_5/Lesson_5-3/Exercise_3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson_5_3
+{
+    class Exercise_3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа:");
+            string s = Console.ReadLine();
+            File.WriteAllBytes("test.txt", CreateByteArray(s));
+        }
+
+        static byte[] CreateByteArray(string text)
+        {
+            byte[] a = new byte[text.Length];
+            int i = 0;
+            foreach (char c in text)
+            {
+                a[i] = Convert.ToByte(c);
+                i++;
+            }
+            return a;
+        }
+    }
+}
