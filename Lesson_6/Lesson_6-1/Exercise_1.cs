a98ed1515eb3a67a2f663a695eccb74af8f9623b
Victor <kaleop@mail.ru>
2021-06-04 21:58:52 +0300

    Шестой урок первое задание

---
+++ Lesson_6/Lesson_6-1/Exercise_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lesson_6_1
+{
+    class Exercise_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущенные процессы:");
+            Process[] allProcesses = Process.GetProcesses();
+            ShowProcessList(allProcesses);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nВведите имя процесса или ID чтобы завершить его");
+            Console.ResetColor();
+            string processKill = Console.ReadLine();
+            Process selectedProcess;
+            int ID = 0;
+            try
+            {
+                if (Int32.TryParse(processKill, out ID) == true)
+                {
+                    selectedProcess = GetProcess(ID, allProcesses);
+                }
+                else
+                {
+                    selectedProcess = GetProcess(processKill, allProcesses);
+                }
+                selectedProcess.Kill();
+            }
+            catch (ProcessNotFound ex) when (ex.Code == Teg.ID)
+            {
+                Console.WriteLine("\nПроцес с таким ID не найден");
+            }
+            catch (ProcessNotFound ex) when (ex.Code == Teg.Name)
+            {
+                Console.WriteLine("\nПроцесс с таким именем не найден");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static void ShowProcessList(Process[] allProcesses)
+        {
+            for (int i = 0; i < allProcesses.Length; i++)
+            {
+                Console.WriteLine($"{allProcesses[i].ProcessName} \t{allProcesses[i].Id}");
+            }
+        }
+
+        static Process GetProcess (string name, Process[] proc)
+        {
+            foreach (Process p in proc )
+            {
+                if (p.ProcessName == name)
+                {
+                    return p;
+                }
+            }
+            throw new ProcessNotFound(Teg.Name);
+        }
+        static Process GetProcess(int ID, Process[] proc)
+        {
+            foreach (Process p in proc)
+            {
+                if (p.Id == ID)
+                {
+                    return p;
+                }
+            }
+            throw new ProcessNotFound(Teg.ID);
+        }
+
+
+
+    }
+}
---
+++ Lesson_6/Lesson_6-1/ProcessNotFound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_1
+{
+    enum Teg
+    {
+        Name, ID
+    }
+
+    [Serializable]
+    class ProcessNotFound : Exception
+    {
+        public Teg Code { get; }
+        public ProcessNotFound (Teg t)
+        {
+            Code = t;
+        }
+    }
+}
